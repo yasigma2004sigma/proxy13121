e6c9a479fd9d3a661f15e37baa6789ec7130475f
yasigma2004sigma <emalenkoivan32@gmail.com>
2024-12-28 22:48:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,61 @@
 import os
+import random
+import string
 
-def main():
-    username = "userproxy"
+def generate_random_string(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for _ in range(length))
+
+def get_password():
     try:
-        password_proxy = input("Type your password here: ")
+        return input("Type your password here: ")
     except:
-        password_proxy = input("Type your password here: ")
+        return input("Type your password here: ")
 
-    os.system("sudo apt-get update")
-    os.system("sudo apt-get -y install build-essential libwrap0-dev libpam0g-dev libkrb5-dev libsasl2-dev")
+def setup_dante(username, password_proxy):
+    os.system("apt-get update")
+    os.system("apt-get -y install build-essential libwrap0-dev libpam0g-dev libkrb5-dev libsasl2-dev")
     os.system("wget --no-check-certificate https://ahmetshin.com/static/dante.tgz")
     os.system("tar -xvpzf dante.tgz")
-    os.system("sudo apt-get -y install libwrap0 libwrap0-dev")
-    os.system("sudo apt-get -y install gcc make")
-    os.system("sudo mkdir -p /etc/danted")
-    os.system("cd dante && ./configure --prefix=/etc/danted && make && sudo make install")
-    os.system("""
-        echo '
-        logoutput: /var/log/danted.log
-        internal: eth0 port = 1080
-        external: eth0
+    os.system("mkdir -p /home/dante")
+    os.system("""cd dante && ./configure --prefix=/home/dante && make && make install""")
+    os.system("""echo '
+    logoutput: syslog /var/log/danted.log
+    internal: eth0 port = 1080
+    external: eth0
 
-        socksmethod: username
-        user.privileged: root
-        user.unprivileged: nobody
+    socksmethod: username
+    user.privileged: root
+    user.unprivileged: nobody
 
-        client pass {
-            from: 0.0.0.0/0 to: 0.0.0.0/0
-            log: error
-        }
+    client pass {
+        from: 0.0.0.0/0 to: 0.0.0.0/0
+        log: error
+    }
 
-        socks pass {
-            from: 0.0.0.0/0 to: 0.0.0.0/0
-            command: connect
-            log: error
-            method: username
-        }' > /etc/danted/danted.conf
-    """)
-    os.system(f"sudo useradd --shell /usr/sbin/nologin -m {username}")
-    os.system(f'echo "{username}:{password_proxy}" | sudo chpasswd')
-    os.system("sudo apt-get -y install ufw")
-    os.system("sudo ufw allow ssh")
-    os.system("sudo ufw allow from any to any port 1080 proto tcp")
-    os.system("sudo ufw enable")
-    os.system("sudo systemctl enable danted")
-    os.system("sudo systemctl start danted")
-    os.system("echo 'Proxy installation successful'")
-    os.system(f"echo 'YOUR IP ADDRESS: `hostname -I | awk '{{print $1}}'`'")
-    os.system("echo 'PORT: 1080'")
-    os.system(f"echo 'LOGIN: {username}'")
-    os.system(f"echo 'PASSWORD: {password_proxy}'")
+    socks pass {
+        from: 0.0.0.0/0 to: 0.0.0.0/0
+        command: connect
+        log: error
+        method: username
+    }' > /home/dante/danted.conf""")
+    os.system("useradd --shell /usr/sbin/nologin -m {}".format(username))
+    os.system('echo "{}:{}" | chpasswd'.format(username, password_proxy))
+    os.system("apt-get -y install ufw")
+    os.system("ufw allow ssh")
+    os.system("ufw allow proto tcp from any to any port 1080")
+    os.system("ufw enable")
+
+def main():
+    username = generate_random_string(12)  # Генерация логина длиной 12 символов
+    password_proxy = get_password()
+    setup_dante(username, password_proxy)
+    print("Proxy installation success")
+    print("\n________________________________\n")
+    print(f"YOUR IP ADDRESS: {os.popen('hostname -I').read().split()[0]}")
+    print(f"PORT: 1080")
+    print(f"LOGIN: {username}")
+    print(f"PASSWORD: {password_proxy}")
 
 if __name__ == "__main__":
     main()
